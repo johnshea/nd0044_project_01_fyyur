9988658baa70e6f656ff6f25fd43b1f09cecc0d7
John Shea <sheajohnh@gmail.com>
2021-12-17 23:49:52 -0500

    Add SQLAlchemy model Show

--- app.py
+++ app.py
@@ -28,6 +28,17 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+class Show(db.Model):
+    __tablename__ = 'shows'
+
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venues.id"), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+      return f'<Show id={self.id}, venue_id={self.venue_id}, artist_id={self.artist_id}, start_time=\'{self.start_time}\'>'
+
 venue_genre_association = db.Table("venue_genre_association",
     db.Column("venue_id", db.Integer, db.ForeignKey("venues.id"), primary_key=True),
     db.Column("genre_id", db.Integer, db.ForeignKey("genres.id"), primary_key=True)
