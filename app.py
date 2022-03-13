6875c9f945947b49004906153977b3d74e1ef3b3
John Shea <sheajohnh@gmail.com>
2022-03-12 21:12:40 -0500

    feat: Implement delete venue endpoint
    Implement delete venu endpoint and cascades on delete in db models
    
    Endpoint:
      DELETE /venues/<venue_id>

--- app.py
+++ app.py
@@ -58,8 +58,9 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String())
-    genres = db.relationship("Genre", secondary=venue_genre_association, backref=db.backref("venues", lazy=True))
+    genres = db.relationship("Genre", secondary=venue_genre_association, backref=db.backref("venues", lazy=True), cascade="all, delete")
     website = db.Column(db.String(120))
+    shows = db.relationship("Show", backref="venue", cascade="all, delete")
 
     def __repr__(self):
       return f'<Venue id={self.id}, name=\'{self.name}\'>'
@@ -79,12 +80,14 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.relationship("Genre", secondary=artist_genre_association, backref=db.backref("artists", lazy=True))
+    genres = db.relationship("Genre", secondary=artist_genre_association, backref=db.backref("artists", lazy=True), cascade="all, delete")
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String())
     website = db.Column(db.String(120))
+    shows = db.relationship("Show", backref="artist", cascade="all, delete")
+
 
     def __repr__(self):
       return f'<Artist id={self.id}, name=\'{self.name}\'>'
@@ -261,12 +264,22 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  venue = Venue.query.get(venue_id)
+
+  if not venue:
+    abort(404)
+
+  try:
+    db.session.delete(venue)
+    db.session.commit()
+  except Exception as e:
+    db.session.rollback()
+  finally:
+    db.session.close()
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  return render_template('pages/home.html')
 
 #  Artists
 #  ----------------------------------------------------------------
